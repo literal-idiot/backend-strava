721f99e72eda63c6bbd354b7dba845c0cd1196dc
michael <michael@Michaels-MacBook-Air-3.local>
2025-07-27 00:12:44 +0800

    Make all user.id to be string when generating access token

--- auth.py
+++ auth.py
@@ -72,7 +72,7 @@ def register():
         db.session.commit()
         
         # Create access token
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=str(user.id))
         
         return jsonify({
             'message': 'User registered successfully',
@@ -108,7 +108,7 @@ def login():
             return jsonify({'error': 'Account is deactivated'}), 401
         
         # Create access token
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=str(user.id))
         
         return jsonify({
             'message': 'Login successful',
